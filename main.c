dd421106d8f471c0682e5be8b0976ced86c6d10c
jay <jay>
2025-05-14 22:37:28 +1000

    Cleaning up and improvements

--- main.c
+++ main.c
@@ -1,33 +1,17 @@
 #include "main.h"
+#include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/ip.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 
-// position in header
-//#define DESTINATION_IP 192.168.20.1
-//#define SOURCD_IP 192.168.20.16
-
-// position in icmp header
-
-// payload index
-//
-//
-// ip_header = b'\x45\x00\x00\x1c' # Version, IHL, Type of Service | Total
-// Length ip_header += b'\xab\xcd\x00\x00' # Identification | Flags, Fragment
-// Offset ip_header += b'\x40\x01\x6b\xd8' # TTL, Protocol | Header Checksum
-// ip_header += b'\xc0\xa8\x92\x83' # Source Address
-// ip_header += b'\x08\x08\x08\x08' # Destination Address
-
-// icmp_header = b'\x08\x00\xe5\xca' # Type of message, Code | Checksum
-// icmp_header += b'\x12\x34\x00\x01' # Identifier | Sequence Number`k_
 uint16_t get_checksum(uint8_t *buffer) {
-  return 0xffff - buffer[ICMP_CODE_INDEX] -
-         buffer[ICMP_TYPE_INDEX]; // - buffer[IDENTIFICATION_INDEX];//flags
-                                  // offsets left out as 0
+  return 0xffff - buffer[ICMP_CODE_INDEX] - buffer[ICMP_TYPE_INDEX];
 }
+
 uint16_t calculate_checksum(unsigned char *buffer, int bytes) {
   uint32_t checksum = 0;
   unsigned char *end = buffer + bytes;
@@ -64,7 +48,8 @@ void u32_to_bytes(uint8_t *bytes, uint32_t value) {
   bytes[2] = value >> 8;
   bytes[3] = value;
 }
-bool create_packet_buffer(uint8_t *buffer, size_t buffer_size) {
+
+void create_packet_buffer(uint8_t *buffer, size_t buffer_size) {
   buffer[ICMP_TYPE_INDEX] = 0x08;
   buffer[ICMP_CODE_INDEX] = 0x00;
   buffer[ICMP_CHECKSUM_INDEX] = 0;
@@ -74,40 +59,68 @@ bool create_packet_buffer(uint8_t *buffer, size_t buffer_size) {
   buffer[ICMP_CHECKSUM_INDEX + 1] = checksum_result;
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != CLI_ARGUMENTS + 1) {
-    printf("Please pass destination ip and payload length\n");
-    printf("argc of :%u\n", argc);
-    printf("argv of :%s\n", argv[1]);
+int fill_args(int argc, const char *argv[], uint32_t *destination_ip,
+              int *packet_size) {
+
+  // Check if '--help' was the first arg
+  if (memcmp(argv[1], "--help", strlen(argv[1])) == 0) {
+    PRINT_STR("Help Prompt");
     return 1u;
   }
 
-  uint8_t buffer[PACKET_MIN_LENGTH];
-  create_packet_buffer(buffer, sizeof(buffer));
+  // Check if too many args
+  if (argc > 3) {
+    PRINT_STR("Too many args");
+    return 1u;
+  }
+
+  // Check if no args passed
+  if (argc == 0) {
+    PRINT_STR("Must pass host target ip");
+    return 1u;
+  }
+
+  // Check the payload size
+  if (argv[2] != NULL) {
+    *packet_size = atoi(argv[2]);
+    if (*packet_size > PACKET_MAX_LENGTH) {
+      PRINT_STR("Packet size is too large");
+      return 1u;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int payload_size = 0;
+  uint32_t destination_ip = 0;
+
+  if (fill_args(argc, argv, &destination_ip, &payload_size) != 0) {
+    return 1u;
+  }
+
+  uint8_t buffer[PACKET_MAX_LENGTH] = {0};
+  create_packet_buffer(buffer, payload_size + PACKET_MIN_LENGTH);
 
   uint16_t port = 0;
   sa_family_t net_type = AF_INET;
 
-  // uint32_t DESTINATION_IP = inet_addr("192.168.1.1");
   uint32_t DESTINATION_IP = inet_addr(argv[1]);
 
   struct sockaddr_in serv_addr;
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = DESTINATION_IP;
 
-  int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP); // ICMP=1 in
-                                                        // /etc/sockets
+  int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
   if (sockfd < 0) {
     printf("Socket Error:%d:%d\n", sockfd, errno);
     return 1;
   }
 
-  printf("size: %u", sizeof(serv_addr));
   int send_status = -2;
-  // for (int i = 0; i < 10; i++) {
   printf("\nSending Now. Press ctrl+c to stop.\n");
   while (1) {
-    send_status = sendto(sockfd, buffer, sizeof(buffer), 0,
+    send_status = sendto(sockfd, buffer, payload_size + PACKET_MIN_LENGTH, 0,
                          (struct sockaddr *)&serv_addr, 32);
     if (send_status < 0) {
       printf("Error with Sending to Socket:%d:%d\n", send_status, errno);
@@ -115,7 +128,6 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // bool send_status = send(sockfd,buffer,sizeof(buffer),NULL);
   printf("\nFinished\n");
   return 0;
 }
--- main.h
+++ main.h
@@ -1,17 +1,22 @@
-#ifndef MAIN 
-#define MAIN 
+#ifndef MAIN
+#define MAIN
 
+#include <stdio.h>
 
+#define PRINT_STR(x) printf("\n%s\n", x)
 #define CLI_ARGUMENTS 2
 
+#define PING_MIN_LENGTH 24u
+
 #define PACKET_MIN_LENGTH 8
+#define PACKET_MAX_LENGTH 8511u
 
 #define ICMP_v4_INDEX = 4
 
 #define ECHO_REQUEST_HEADER_BIT = 8;
 
 #define ICMP_TYPE_INDEX 0
-#define ICMP_CODE_INDEX 1 
+#define ICMP_CODE_INDEX 1
 #define ICMP_CHECKSUM_INDEX 2
 #define ICMP_IDENTIFIER_INDEX 4
 #define ICMP_SEQUENCE_NUMBER_INDEX 6
