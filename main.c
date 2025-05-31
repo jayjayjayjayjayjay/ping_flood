9354af5a5da08329e8056e9e149a624eda829e0d
jay <jay>
2025-06-01 00:17:36 +1000

    Implement calculate checksum
    master
    Implemented from logic on wikipedia
    Tested checksum as valid via wireshark (loopback)
    Tested against wikipedia example too

--- main.c
+++ main.c
@@ -12,34 +12,18 @@ uint16_t get_checksum(uint8_t *buffer) {
   return 0xffff - buffer[ICMP_CODE_INDEX] - buffer[ICMP_TYPE_INDEX];
 }
 
-uint16_t calculate_checksum(unsigned char *buffer, int bytes) {
-  uint32_t checksum = 0;
-  unsigned char *end = buffer + bytes;
+uint16_t calculate_checksum(uint8_t *buffer, size_t buffer_size) {
+  uint32_t checksum = 0u;
 
-  // odd bytes add last byte and reset end
-  if (bytes % 2 == 1) {
-    end = buffer + bytes - 1;
-    checksum += (*end) << 8;
+  for (size_t idx = 0u; idx < 10u; idx++) {
+    if (idx == 5) {
+      continue;
+    }
+    checksum += (buffer[idx * 2] << 8) | buffer[idx * 2 + 1];
   }
 
-  // add words of two bytes, one by one
-  while (buffer < end) {
-    checksum += buffer[0] << 8;
-    checksum += buffer[1];
-    buffer += 2;
-  }
-
-  // add carry if any
-  uint32_t carray = checksum >> 16;
-  while (carray) {
-    checksum = (checksum & 0xffff) + carray;
-    carray = checksum >> 16;
-  }
-
-  // negate it
-  checksum = ~checksum;
-
-  return checksum & 0xffff;
+  checksum = ((checksum & 0xf0000) >> 16) + (checksum & 0xffff);
+  return ~((uint16_t)checksum);
 }
 
 void u32_to_bytes(uint8_t *bytes, uint32_t value) {
@@ -88,6 +72,7 @@ int fill_args(int argc, const char *argv[], uint32_t *destination_ip,
       return 1u;
     }
   }
+  return 0u;
 }
 
 int main(int argc, char *argv[]) {
